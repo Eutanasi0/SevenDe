5f4d69806c9d80208e6d68108b843488bef903e6
Eutanasi0 <benjaminflores4e@gmail.com>
2024-01-24 20:39:29 -0500

    elhtml

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("hi, im wasa");
+})
+
 app.listen(3000, () => {
     console.log("hi, im wasa and im in localhost", 3000);
 });
