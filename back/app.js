fd6cedea454f82d07b5bc0e6f6231816d014c3a1
Eutanasi0 <benjaminflores4e@gmail.com>
2024-02-08 16:20:24 -0500

    server

--- back/app.js
+++ back/app.js
@@ -1,4 +1,23 @@
 const express = require("express");
+
+
+const app = express();
+
+
+
+app.use(express.static('front'));
+
+app.listen(3000, () => {
+    console.log('hi, im wasa and im innn http://localhost:3000');
+});
+
+
+// Iniciar el servidor
+/*
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
+const express = require("express");
 const path = require("path");
 
 const app = express();
@@ -10,6 +29,4 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("hi, im wasa and im innn localhost", 3000);
 });
-console.log("Hola");
-console.log("chau");
-console.log("hola");
+*/
