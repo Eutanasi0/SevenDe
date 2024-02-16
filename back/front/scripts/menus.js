6c58237439590f28125d5e5a0a57888bb840c8d6
Eutanasi0 <benjaminflores4e@gmail.com>
2024-02-15 19:32:12 -0500

    js

---
+++ back/front/scripts/menus.js
@@ -0,0 +1,49 @@
+const jsonComidas = {
+    "listasComidas": [
+      {
+        "tipo": "menu 1",
+        "platos": ["panqueques", "huevos revueltos", "tostadas"],
+        "platodefondo": ["ensalada", "sopa", "pollo asado"]
+      },
+      {
+        "tipo": "menu 2",
+        "platos": ["ensalada", "sopa", "pollo asado"],
+        "platodefondo": ["panqueques", "huevos revueltos", "tostadas"]
+
+      },
+      {
+        "tipo": "menu 3",
+        "platos": ["pescado a la parrilla", "ensalada", "arroz"],
+        "platodefondo": ["panqueques", "huevos revueltos", "tostadas"]
+      }
+    ]
+  };
+
+  const menusContainer = document.querySelector('.menus-container');
+
+  jsonComidas.listasComidas.forEach(lista => {
+      const menuDiv = document.createElement('div');
+      menuDiv.classList.add('menus');
+      const tipoTitulo = document.createElement('h3');
+      tipoTitulo.textContent = lista.tipo.charAt(0).toUpperCase() + lista.tipo.slice(1);
+      const platosUl = document.createElement('ul');
+
+      lista.platos.forEach(plato => {
+          const platoLi = document.createElement('li');
+          platoLi.textContent = "Entrada: " + plato;
+          platosUl.appendChild(platoLi);
+      });
+
+      const platoFondoUl = document.createElement('ul');
+      lista.platodefondo.forEach(plato => {
+          const platoLi = document.createElement('li');
+          platoLi.textContent = "Segundo : " + plato;
+          platoFondoUl.appendChild(platoLi);
+      });
+
+      menuDiv.appendChild(tipoTitulo);
+      menuDiv.appendChild(platosUl);
+      menuDiv.appendChild(platoFondoUl);
+      menusContainer.appendChild(menuDiv);
+  });
+
