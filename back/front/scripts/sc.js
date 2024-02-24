23712208055c979c1dfeb007879628e66c2415e9
Eutanasi0 <benjaminflores4e@gmail.com>
2024-02-24 10:27:11 -0500

    sr

--- back/front/scripts/sc.js
+++ back/front/scripts/sc.js
@@ -7,3 +7,7 @@ const sr = ScrollReveal({
 
 sr.reveal('.txt',{delay: 200, origin:'right'});
 sr.reveal('.img',{delay: 200, origin:'left'});
+sr.reveal('.slogan',{delay: 200, origin:'bottom'});
+sr.reveal('#c1',{delay: 300, origin:'top'});
+sr.reveal('#c2',{delay: 350, origin:'top'});
+sr.reveal('#c3',{delay: 400, origin:'top'});
